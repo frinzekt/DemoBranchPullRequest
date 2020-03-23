c816026944a09e2995c40a06092d9870fd15b38f
frinzekt <frinzekt@gmail.com>
2020-03-23 23:41:49 +0800

    basic layout and task

---
+++ main.py
@@ -0,0 +1,6 @@
+import operations as op
+
+print(op.add(4,2))
+print(op.mult(4,2))
+print(op.sub(4,2))
+print(op.div(4,2))
---
+++ operations.py
@@ -0,0 +1,19 @@
+# STUB
+# Adds x and y
+def add(x, y):
+    return 0
+
+# STUB
+# Multiplies x and y
+def mult(x, y):
+    return 0
+
+# STUB
+# Subtracts x from y
+def sub(x, y):
+    return 0
+
+# STUB
+# Divides two numbers
+def div(x, y):
+    return 0
